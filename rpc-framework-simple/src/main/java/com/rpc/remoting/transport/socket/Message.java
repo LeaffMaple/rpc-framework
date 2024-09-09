5cc6cb96a74f0c58a41c482031569e87d18c6609
maple <2743949057@qq.com>
2024-09-09 11:30:09 +0800

    update: socket测试

---
+++ rpc-framework-simple/src/main/java/com/rpc/remoting/transport/socket/Message.java
@@ -0,0 +1,13 @@
+package com.rpc.remoting.transport.socket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Message implements Serializable {
+
+    private String content;
+}
